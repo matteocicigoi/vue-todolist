a117b90f7c624236082bd25983e4a523243d15d4
matteocicigoi <matteo.cicigoi1@hotmail.it>
2023-11-28 14:29:57 +0100

    feat: milestone 1

--- js/main.js
+++ js/main.js
@@ -5,7 +5,20 @@ const { createApp } = Vue;
   createApp({
     data() {
       return {
-        
+        list: [
+            {
+                text: 'Todo 1',
+                done: false
+            },
+            {
+                text: 'Todo 2',
+                done: false
+            },
+            {
+                text: 'Todo 3',
+                done: true
+            }
+        ]
       }
     }
   }).mount('#app');
